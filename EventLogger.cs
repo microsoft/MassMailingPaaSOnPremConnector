b8f82ba205f8947bb27fa9ccc2ef8cf3410d1996
Tommaso Toniolo <totoni@microsoft.com>
2024-11-19 20:59:44 +0100

    Add project files.

---
+++ EventLogger.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security;
+using System.Text;
+
+namespace ACSOnPremConnector
+{
+    /*
+     * Provides a simple logging mechanism to write to the Windows Event Log.
+     * This will try to create a new Event Source (defined by eventSource) if it does not exist, if the operation fails, it will default to "Application".
+     * For the creation of the Event Source, the user running the application must have the necessary permissions to create a new Event Source (requires Admin privileges).
+     * The logging will be attempted on the custom Event Source (as defined on eventSource), if it fails, writing will be attempted on ACSOnPremConnector, if that fails again it will default to "Application".
+     */
+    internal class EventLogger : IDisposable
+    {
+        private string Source = String.Empty;
+        private StringBuilder EventLogMessage = null;
+
+        public EventLogger(string eventSource = "Application")
+        {
+            EventLogMessage = new StringBuilder();
+
+            try
+            {
+                Source = eventSource;
+                if (!EventLog.SourceExists(Source))
+                {
+                    EventLog.CreateEventSource(Source, "Application");
+                }
+            }
+            catch (SecurityException)
+            {
+                try
+                {
+                    Source = "ACSOnPremConnector";
+                    if (!!EventLog.SourceExists(Source))
+                    {
+                        EventLog.CreateEventSource(Source, "Application");
+                    }
+                }
+                catch (SecurityException)
+                {
+                    Source = "Application";
+                    if (!EventLog.SourceExists(Source))
+                    {
+                        EventLog.CreateEventSource(Source, "Application");
+                    }
+                }
+            }
+        }
+
+        public void LogDebug(bool isDebugEnabled = true)
+        {
+            if (isDebugEnabled)
+            {
+                EventLog.WriteEntry(Source, EventLogMessage.ToString(), EventLogEntryType.Information);
+            }
+            EventLogMessage.Clear();
+        }
+
+        public void LogInformation()
+        {
+            EventLog.WriteEntry(Source, EventLogMessage.ToString(), EventLogEntryType.Information);
+            EventLogMessage.Clear();
+        }
+
+        public void LogWarning()
+        {
+            EventLog.WriteEntry(Source, EventLogMessage.ToString(), EventLogEntryType.Warning);
+            EventLogMessage.Clear();
+        }
+
+        public void LogError()
+        {
+            EventLog.WriteEntry(Source, EventLogMessage.ToString(), EventLogEntryType.Error);
+            EventLogMessage.Clear();
+        }
+
+        public void LogException()
+        {
+            EventLog.WriteEntry(Source, EventLogMessage.ToString(), EventLogEntryType.Error);
+            EventLogMessage.Clear();
+        }
+
+        public void AppendLogEntry(string message)
+        {
+            EventLogMessage.AppendLine(message);
+        }
+
+        public void AppendLogEntry(Exception ex)
+        {
+            EventLogMessage.AppendLine("--------------------------------------------------------------------------------");
+            EventLogMessage.AppendLine(String.Format("EXCEPTION MESSAGE: {0}", ex.Message));
+            EventLogMessage.AppendLine(String.Format("EXCEPTION HRESULT: {0}", ex.HResult));
+            EventLogMessage.AppendLine(String.Format("EXCEPTION SOURCE: {0}", ex.Source));
+            EventLogMessage.AppendLine(String.Format("EXCEPTION INNER EXCEPTION: {0}", ex.InnerException));
+            EventLogMessage.AppendLine(String.Format("EXCEPTION STRACK: {0}", ex.StackTrace));
+            EventLogMessage.AppendLine("--------------------------------------------------------------------------------");
+            EventLogMessage.AppendLine(ex.ToString());
+            EventLogMessage.AppendLine("--------------------------------------------------------------------------------");
+        }
+
+        public void AppendLogEntry(object obj)
+        {
+            EventLogMessage.AppendLine(obj.ToString());
+        }
+
+        ~EventLogger()
+        {
+            WriteEventLogOnExit();
+        }
+
+        void IDisposable.Dispose()
+        {
+            WriteEventLogOnExit();
+        }
+
+        private void WriteEventLogOnExit()
+        {
+            if (!String.IsNullOrEmpty(EventLogMessage.ToString()))
+            {
+                EventLogMessage.AppendLine("Writing Event on Agent exit");
+                EventLog.WriteEntry(Source, EventLogMessage.ToString(), EventLogEntryType.Information);
+                EventLogMessage.Clear();
+            }
+            EventLogMessage = null;
+        }
+
+    }
+}
---
+++ TextLogger.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+namespace ACSOnPremConnector
+{
+    /*
+     * Provides a simple logging mechanism to write to a text file. The text fille will be stored on logLocation and named logName.
+     * It is imperative that this log file is excluded from any monitoring or anti-virus software, as it will be written to constantly.
+     */
+    internal class TextLogger : IDisposable
+    {
+        private string _logPath = string.Empty;
+        private StreamWriter _logStream = null;
+
+        public TextLogger(string logLocation, string logName)
+        {
+            string logPath = Path.Combine(logLocation, logName);
+
+            if (_logPath != logPath)
+            {
+                CloseStream();
+            }
+
+            try
+            {
+                _logPath = logPath;
+                _logStream = new StreamWriter(_logPath, true);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public TextLogger(string logPath)
+        {
+            if (_logPath != logPath)
+            {
+                CloseStream();
+            }
+
+            try
+            {
+                _logPath = logPath;
+                _logStream = new StreamWriter(_logPath, true);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        ~TextLogger()
+        {
+            CloseStream();
+        }
+
+        void IDisposable.Dispose()
+        {
+            CloseStream();
+        }
+        private void CloseStream()
+        {
+            if (_logStream != null)
+            {
+                _logStream.Flush();
+                _logStream.Dispose();
+            }
+        }
+
+        public void WriteToText(string message)
+        {
+            _logStream.WriteLine(String.Format("{0:yyyy-MM-dd HH:mm:ss} | {1}", DateTime.Now, message));
+            _logStream.Flush();
+        }
+
+    }
+}
